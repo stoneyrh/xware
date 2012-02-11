4681e20d4d3c20a9304e80aa7a77fffac22688df
Ronghui Yu <stoneyrh@163.com>
2012-02-11 21:34:41 +0800

    rename SConscript to xagent.scons, change build_xagent_all.py

--- build_xagent_all.py
+++ build_xagent_all.py
@@ -2,10 +2,10 @@ import os
 import sys
 
 commands = [
-        'scons unicode=yes release=no',
-        'scons unicode=no release=no',
-        'scons unicode=yes release=yes',
-        'scons unicode=no release=yes']
+        'scons -f xagent.scons unicode=yes release=no',
+        'scons -f xagent.scons unicode=no release=no',
+        'scons -f xagent.scons unicode=yes release=yes',
+        'scons -f xagent.scons unicode=no release=yes']
 
 if __name__ == '__main__':
     for command in commands:
