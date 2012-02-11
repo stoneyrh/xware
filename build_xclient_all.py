dc74298f3edfe3778dd2f76bae7d694b098d49e9
Ronghui Yu <stoneyrh@163.com>
2012-02-11 21:33:06 +0800

    add xclient, this is used for testing communication of xnetwork

---
+++ build_xclient_all.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+commands = [
+        'scons -f xclient.scons unicode=yes release=no',
+        'scons -f xclient.scons unicode=no release=no',
+        'scons -f xclient.scons unicode=yes release=yes',
+        'scons -f xclient.scons unicode=no release=yes']
+
+if __name__ == '__main__':
+    for command in commands:
+        os.system(' '.join([command] + sys.argv[1:]))
