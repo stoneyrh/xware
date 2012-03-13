f9c8b1611dff87948ec9d31920667b87a16ca181
Ronghui Yu <stoneyrh@163.com>
2012-03-13 15:00:34 +0800

    commited by xgit.py

---
+++ xbuilder/xgit.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+git = u'git'
+
+def commit(file_name, message = None):
+    if not message:
+        message = u'commited by xgit.py'
+    command = u'%s commit %s -m "%s"' % (git, file_name, message)
+    return os.system(command) == 0
+
+def push():
+    command = u'%s push' % git
+    return os.system(command) == 0
+
+def main():
+    if len(sys.argv) < 2:
+        print 'Usage: xgit.py <file> [<message>]'
+    else:
+        file_name = sys.argv[1]
+        message = None
+        if len(sys.argv) == 3:
+            message = sys.argv[2]
+        commit(file_name, message)
+
+if __name__ == u'__main__':
+    main()
